fcc07132ee4425eed23e6a8ec9b0191d2a14c9ae
Reza Ghanbari <rezapython@gmail.com>
2017-02-16 01:09:25 +0330

    building the server 2

--- src/main.go
+++ src/main.go
@@ -11,7 +11,11 @@ var clients = make(map[*websocket.Conn]bool)  // connected clients
 var broadcast = make(chan Message) // broadcast channel
 
 // configure the upgrader
-var upgrader = websocket.Upgrader{}
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
 
 // define our message object
 type Message struct {
@@ -27,5 +31,57 @@ func main()  {
 
 	// configure websocket route
 	http.HandleFunc("/ws", handleConnections)
+	go handleMessages()
+
+	// start the server on localhost port 8000 and log any errors
+	log.Println("http server started on :8000")
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
 
+func handleConnections(w http.ResponseWriter, r *http.Request)  {
+	// upgrade initial GET request to a websocket
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// make sure we close the connection when the function returns
+	defer ws.Close()
+
+	// register our new client
+	clients[ws] = true
+
+	for {
+		var msg Message
+	        // Read in a new message as JSON and map it to a Message object
+
+		err := ws.ReadJSON(&msg)
+		if err != nil {
+			log.Printf("error: %v", err)
+                        delete(clients, ws)
+                        break
+		}
+
+                // Send the newly received message to the broadcast channel
+		broadcast <- msg
+	}
+}
+
+func handleMessages()  {
+	for {
+		// Grab the next message from the broadcast channel
+		msg := <-broadcast
+		// Send it out to every client that is currently connected
+		for client := range clients {
+			err := client.WriteJSON(msg)
+			if err != nil {
+				log.Printf("error: %v", err)
+				client.Close()
+				delete(clients, client)
+			}
+		}
+	}
+}
